70cd39066c79578708d41aeb1fcf94d2df2e0876
raworiginal <randall.a.wolfe@gmail.com>
2025-07-18 09:52:30 -0700

    connected to the mogoDB database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 /* ====================== Modules ======================*/
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
 
 /* ====================== Constants ======================*/
 const app = express();
@@ -11,6 +14,12 @@ app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 /* ====================== Listener ======================*/
+mongoose.connect(process.env.MONGODB_URI);
+//log conneciton status in terminal at start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
