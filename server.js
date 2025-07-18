42142c7b136cdb243457077d73b48e9745ba52e9
raworiginal <randall.a.wolfe@gmail.com>
2025-07-18 10:55:16 -0700

    created the new fruit form in views/fruits/new.ejs

--- server.js
+++ server.js
@@ -4,25 +4,30 @@ const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
 const Fruit = require("./models/fruit.js");
+/* ====================== Middleware ======================*/
 /* ====================== Constants ======================*/
 const app = express();
 /* ====================== Variables ======================*/
+
 /* ====================== Functions ======================*/
 
+/* ====================== Query Functions ======================*/
+
+/* ====================== DB Connection ======================*/
+mongoose.connect(process.env.MONGODB_URI);
+//log conneciton status in terminal at start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
 /* ====================== Routes ======================*/
 app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 
 app.get("/fruits/new", (req, res) => {
-  res.send("This route sends the user a form page!");
+  res.render("fruits/new.ejs");
 });
-/* ====================== Listener ======================*/
-mongoose.connect(process.env.MONGODB_URI);
-//log conneciton status in terminal at start
-mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
-});
+/* ====================== Server ======================*/
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
